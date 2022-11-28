a96b2c8b8b5ee940f1ab09aef8d6fcf4eed7d91f
kushagr-awasthi <112492988+kushagr-awasthi@users.noreply.github.com>
2022-11-28 15:20:00 -0500

    extracred entry added

---
+++ Entries/extracred/script.js
@@ -0,0 +1,142 @@
+$(document).ready(function(){
+    $(".A").click(function(){
+        if ($(".A").hasClass("orange")) {
+            $(".A").removeClass("orange");
+            $(".A").addClass("red");
+          } 
+        else if ($(".A").hasClass("red")) {
+            $(".A").removeClass("red");
+            $(".A").addClass("green");
+          }
+        else if ($(".A").hasClass("green")) {
+            $(".A").removeClass("green");
+            $(".A").addClass("yellow");
+          }
+        else if ($(".A").hasClass("yellow")) {
+            $(".A").removeClass("yellow");
+            $(".A").addClass("blue");
+          }
+        else if ($(".A").hasClass("blue")) {
+            $(".A").removeClass("blue");
+            $(".A").addClass("purple");
+          } 
+        else if ($(".A").hasClass("purple")) {
+            $(".A").removeClass("purple");
+            $(".A").addClass("orange");
+          }  
+    });
+
+
+    $(".B").click(function(){
+        if ($(".B").hasClass("orange")) {
+            $(".B").removeClass("orange");
+            $(".B").addClass("purple");
+          } 
+        else if ($(".B").hasClass("purple")) {
+            $(".B").removeClass("purple");
+            $(".B").addClass("blue");
+          }
+        else if ($(".B").hasClass("blue")) {
+            $(".B").removeClass("blue");
+            $(".B").addClass("green");
+          }
+        else if ($(".B").hasClass("green")) {
+            $(".B").removeClass("green");
+            $(".B").addClass("yellow");
+          }
+        else if ($(".B").hasClass("yellow")) {
+            $(".B").removeClass("yellow");
+            $(".B").addClass("red");
+          } 
+        else if ($(".B").hasClass("red")) {
+            $(".B").removeClass("red");
+            $(".B").addClass("orange");
+          }  
+    });
+
+
+    $(".C").click(function(){
+        if ($(".C").hasClass("orange")) {
+            $(".C").removeClass("orange");
+            $(".C").addClass("blue");
+          } 
+        else if ($(".C").hasClass("blue")) {
+            $(".C").removeClass("blue");
+            $(".C").addClass("green");
+          }
+        else if ($(".C").hasClass("green")) {
+            $(".C").removeClass("green");
+            $(".C").addClass("red");
+          }
+        else if ($(".C").hasClass("red")) {
+            $(".C").removeClass("red");
+            $(".C").addClass("purple");
+          }
+        else if ($(".C").hasClass("purple")) {
+            $(".C").removeClass("purple");
+            $(".C").addClass("yellow");
+          } 
+        else if ($(".C").hasClass("yellow")) {
+            $(".C").removeClass("yellow");
+            $(".C").addClass("orange");
+          }  
+    });
+
+
+    $(".D").click(function(){
+        if ($(".D").hasClass("orange")) {
+            $(".D").removeClass("orange");
+            $(".D").addClass("yellow");
+          } 
+        else if ($(".D").hasClass("yellow")) {
+            $(".D").removeClass("yellow");
+            $(".D").addClass("red");
+          }
+        else if ($(".D").hasClass("red")) {
+            $(".D").removeClass("red");
+            $(".D").addClass("blue");
+          }
+        else if ($(".D").hasClass("blue")) {
+            $(".D").removeClass("blue");
+            $(".D").addClass("green");
+          }
+        else if ($(".D").hasClass("green")) {
+            $(".D").removeClass("green");
+            $(".D").addClass("purple");
+          } 
+        else if ($(".D").hasClass("purple")) {
+            $(".D").removeClass("purple");
+            $(".D").addClass("orange");
+          }  
+    });
+
+    $(".E").click(function(){
+        if ($(".E").hasClass("orange")) {
+            $(".E").removeClass("orange");
+            $(".E").addClass("green");
+          } 
+        else if ($(".E").hasClass("green")) {
+            $(".E").removeClass("green");
+            $(".E").addClass("yellow");
+          }
+        else if ($(".E").hasClass("yellow")) {
+            $(".E").removeClass("yellow");
+            $(".E").addClass("purple");
+          }
+        else if ($(".E").hasClass("purple")) {
+            $(".E").removeClass("purple");
+            $(".E").addClass("red");
+          }
+        else if ($(".E").hasClass("red")) {
+            $(".E").removeClass("red");
+            $(".E").addClass("blue");
+          } 
+        else if ($(".E").hasClass("blue")) {
+            $(".E").removeClass("blue");
+            $(".E").addClass("orange");
+          }  
+    });
+
+    
+
+});
