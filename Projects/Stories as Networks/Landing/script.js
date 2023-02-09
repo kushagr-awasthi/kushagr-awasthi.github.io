d3b603077a50324586afcbd32699ec7ed3ebb74e
kushagr-awasthi <112492988+kushagr-awasthi@users.noreply.github.com>
2023-02-08 22:01:27 -0500

    Added Stories as Network- Home Page
    Added Home Page
    
    Responsiveness finetuning is pending for tablet and mobile layouts.
    
    JS for table of contents animation is pending.
    
    JS for mobile/tablet reveal animations is pending.

---
+++ Projects/Stories as Networks/Landing/script.js
@@ -0,0 +1,17 @@
+sesame.onclick = function() {
+    beginning.classList.toggle('animate-beg');
+    ending.classList.toggle("animate-end");
+    sesame.style.display = 'none';
+  };
+
+beginning.onclick = function() {
+    beginning.classList.toggle('animate-beg');
+    ending.classList.toggle("animate-end");
+    sesame.style.display = 'none';
+  };
+
+  ending.onclick = function() {
+    beginning.classList.toggle('animate-beg');
+    ending.classList.toggle("animate-end");
+    sesame.style.display = 'none';
+  };
