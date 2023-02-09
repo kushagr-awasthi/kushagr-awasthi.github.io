700d2345b9c66704fdab2e63b4d03610b580e88f
kushagr-awasthi <112492988+kushagr-awasthi@users.noreply.github.com>
2023-02-09 10:32:01 -0500

    Class Asignment Int Std

--- Projects/Stories as Networks/Landing/script.js
+++ Projects/Stories as Networks/Landing/script.js
@@ -1,17 +1,48 @@
 sesame.onclick = function() {
-    beginning.classList.toggle('animate-beg');
-    ending.classList.toggle("animate-end");
+    beginning.classList.toggle('animate-beg-mob');
+    ending.classList.toggle("animate-end-mob");
     sesame.style.display = 'none';
   };
 
 beginning.onclick = function() {
-    beginning.classList.toggle('animate-beg');
-    ending.classList.toggle("animate-end");
+    beginning.classList.toggle('animate-beg-mob');
+    ending.classList.toggle("animate-end-mob");
     sesame.style.display = 'none';
   };
 
   ending.onclick = function() {
-    beginning.classList.toggle('animate-beg');
-    ending.classList.toggle("animate-end");
+    beginning.classList.toggle('animate-beg-mob');
+    ending.classList.toggle("animate-end-mob");
     sesame.style.display = 'none';
   };
+
+function myFunction(x) {
+    if (x.matches) {
+      
+    } else {
+      sesame.onclick = function() {
+        beginning.classList.toggle('animate-beg');
+        ending.classList.toggle("animate-end");
+        sesame.style.display = 'none';
+      };
+    
+    beginning.onclick = function() {
+        beginning.classList.toggle('animate-beg');
+        ending.classList.toggle("animate-end");
+        sesame.style.display = 'none';
+      };
+    
+      ending.onclick = function() {
+        beginning.classList.toggle('animate-beg');
+        ending.classList.toggle("animate-end");
+        sesame.style.display = 'none';
+      };
+    }
+  }
+  
+
+
+  var x = window.matchMedia("(max-width: 800px)")
+  
+  myFunction(x)
+  x.addListener(myFunction)
