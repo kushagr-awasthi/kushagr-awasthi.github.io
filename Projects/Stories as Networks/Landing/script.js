18d62e83fd79e83914925bd90102119c730d161c
kushagr-awasthi <112492988+kushagr-awasthi@users.noreply.github.com>
2023-02-12 20:23:59 -0500

    Added AAOF

--- Projects/Stories as Networks/Landing/script.js
+++ Projects/Stories as Networks/Landing/script.js
@@ -1,23 +1,34 @@
-sesame.onclick = function() {
-    beginning.classList.toggle('animate-beg-mob');
-    ending.classList.toggle("animate-end-mob");
-    sesame.style.display = 'none';
-  };
+let beginning = document.querySelector("#beginning");
+let ending = document.querySelector("#ending");
+let sesame = document.querySelector("#sesame ");
+var x = window.matchMedia("(max-width: 800px)")
+myFunction(x)
+x.addListener(myFunction)
 
-beginning.onclick = function() {
-    beginning.classList.toggle('animate-beg-mob');
-    ending.classList.toggle("animate-end-mob");
-    sesame.style.display = 'none';
-  };
 
-  ending.onclick = function() {
-    beginning.classList.toggle('animate-beg-mob');
-    ending.classList.toggle("animate-end-mob");
-    sesame.style.display = 'none';
-  };
+
 
 function myFunction(x) {
     if (x.matches) {
+
+      sesame.onclick = function() {
+        beginning.classList.toggle('animate-beg-mob');
+        ending.classList.toggle("animate-end-mob");
+        sesame.style.display = 'none';
+      };
+    
+    beginning.onclick = function() {
+        beginning.classList.toggle('animate-beg-mob');
+        ending.classList.toggle("animate-end-mob");
+        sesame.style.display = 'none';
+      };
+    
+      ending.onclick = function() {
+        beginning.classList.toggle('animate-beg-mob');
+        ending.classList.toggle("animate-end-mob");
+        sesame.style.display = 'none';
+      };
+    
       
     } else {
       sesame.onclick = function() {
@@ -42,7 +53,5 @@ function myFunction(x) {
   
 
 
-  var x = window.matchMedia("(max-width: 800px)")
+
   
-  myFunction(x)
-  x.addListener(myFunction)
