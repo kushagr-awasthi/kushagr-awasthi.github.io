f3e169c12cd12c891bf1ba0fbc9ee5a456ce0ddf
kushagr-awasthi <112492988+kushagr-awasthi@users.noreply.github.com>
2023-03-13 19:21:08 -0400

    Added Pages and JS
    JS is probably all wrong.
    also incomplete.
    
    CSS 90% (maybe add animation)
    try page transitions with swup.

--- Projects/elasticcollection/js/script.js
+++ Projects/elasticcollection/js/script.js
@@ -1,12 +1,465 @@
-fetch("/data.json").then(function (response) {
-  return response.json();
-})
+const dataJSON = [
 
-.then(function(data){
-    let placeholder = document.querySelector("#data-output");
-    let out = "";
-    for(let x of data){
-        out += `<div class="dataname">${x.name}</div>`;
+    {
+        "title":"Ajrakh Blockprints",
+        "Type":"Craft",
+        "Creator":"Khatri Community",
+        "Year":"1590",
+        "Relevance":"Craft Culture",
+        "Intensity":1,
+        "imgURL":"assets/ab.jpg",
+        "readMore":"https://en.wikipedia.org/wiki/Ajrak"
+    },
+
+    {
+        "title":"Folio from the Baburnama",
+        "Type":"Manuscript",
+        "Creator":"Unknown Artist",
+        "Year":"1590",
+        "Relevance":"Aesthetic Heritage",
+        "Intensity":3,
+        "imgURL":"assets/ffb.jpg",
+        "readMore":"https://www.luhringaugustine.com/exhibitions/court-epic-spirit-indian-art-15th-19th-century#tab:thumbnails"
+    },
+
+    {
+        "title":"Rajput Miniature Paintingl",
+        "Type":"Traditional Art",
+        "Creator":"Unknown Artist",
+        "Year":"1840",
+        "Relevance":"Aesthetic Heritage",
+        "Intensity":3,
+        "imgURL":"assets/rmp.jpg",
+        "readMore":"https://www.luhringaugustine.com/exhibitions/court-epic-spirit-indian-art-15th-19th-century#tab:thumbnails"
+        
+    },
+
+    {
+        "title":"Victoria Terminus",
+        "Type":"Architecture",
+        "Creator":["Frederick William Stevens", "Axel Haig"],
+        "Year":"1888",
+        "Relevance":"Colonial Imagery",
+        "Intensity":2,
+        "imgURL":"assets/vt.jpg",
+        "readMore":"https://whc.unesco.org/en/list/945/"
+    },
+
+    {
+        "title":"Bharat Mata",
+        "Type":"Painting",
+        "Creator":"Abanindranath Tagore",
+        "Year":"1905",
+        "Relevance":"Spirit",
+        "Intensity":3,
+        "imgURL":"assets/bm.jpg",
+        "readMore":"https://thecultural.me/abanindranath-tagores-bharat-mata-and-its-role-in-fostering-indian-identity-200413"
+    },
+
+    {
+        "title":"The Dream",
+        "Type":"Painting",
+        "Creator":"Henri Rousseau",
+        "Year":"1910",
+        "Relevance":"Primitivism",
+        "Intensity":2,
+        "imgURL":"assets/td.jpg",
+        "readMore":"https://www.moma.org/collection/works/79277"
+    },
+
+     
+    {
+        "title":"D. B. House",
+        "Type":"Architecture",
+        "Creator":"Uknown Designer",
+        "Year":"1913",
+        "Relevance":"Colonial Imagery",
+        "Intensity":2,
+        "imgURL":"assets/dbh.jpg",
+        "readMore":"https://imagewrighter.com/db-house-the-former-tata-palace-mumbai/"
+    },
+
+    {
+        "title":"Gandhiji: Linocut Poster",
+        "Type":"Poster",
+        "Creator":"Nandalal Bose",
+        "Year":"1930",
+        "Relevance":"Visual Activism",
+        "Intensity":4,
+        "imgURL":"assets/glp.jpg",
+        "readMore":"https://www.christies.com/en/lot/lot-5716143"
+    },
+
+    {
+        "title":"Haripura Poster",
+        "Type":"Poster",
+        "Creator":"Nandalal Bose",
+        "Year":"1937",
+        "Relevance":"Visual Activism",
+        "Intensity":2,
+        "imgURL":"assets/hp.jpg",
+        "readMore":"https://artsandculture.google.com/story/rediscovering-haripura-panels-nandalal-bose-national-gallery-of-modern-art-ngma-new-delhi/AwXRNpSFHeWVJw?hl=en"
+    },
+
+    {
+        "title":"Commemorative Front Page, The Hindu",
+        "Type": "News Graphic",
+        "Creator": "Unknown Designer",
+        "Year": "1947",
+        "Relevance": "Civic Identity",
+        "Intensity": 6,
+        "imgURL":"assets/hfp.jpg",
+        "readMore":"https://decolonisingthroughcriticallibrarianship.wordpress.com/2019/09/13/freedom-and-fragmentation-images-of-independence-decolonisation-and-partition-at-cambridge-university/"
+    },
+
+    {
+        "title":"Illustrations in the original manuscript of the Constitution of India",
+        "Type":"Illustration",
+        "Creator":"Nandalal Bose",
+        "Year":"1950",
+        "Relevance":"Civic Identity",
+        "Intensity":6,
+        "imgURL":"assets/coi.jpg",
+        "readMore":"https://culturalsamvaad.com/constitution-of-india-22-paintings-ode-to-indian-culture-and-history/"
+    },
+
+    {
+        "title":"Painting 4",
+        "Type":"Painting",
+        "Creator":"Vasudeo Gaitonde",
+        "Year":"1962",
+        "Relevance":"Spirit",
+        "Intensity":1,
+        "imgURL":"assets/p4.jpg",
+        "readMore":"https://www.moma.org/magazine/articles/609"
+    },
+
+    {
+        "title":"The Palace of Assembly",
+        "Type":"Architecture",
+        "Creator":"Le Corbusier",
+        "Year":"1965",
+        "Relevance":"Post-Independence Westernisation",
+        "Intensity":4,
+        "imgURL":"assets/tpa.jpg",
+        "readMore":"https://www.architectural-review.com/buildings/the-assembly-chandigarh"
+    },
+
+    {
+        "title":"The Amul Girl",
+        "Type":"Brand Mascot",
+        "Creator":"Eustace Fernandes",
+        "Year":"1966",
+        "Relevance":"Indian Advertising",
+        "Intensity":4,
+        "imgURL":"assets/tag.jpg",
+        "readMore":"https://musebycl.io/advertising/story-amul-girl-indias-ad-icon-who-was-ahead-her-time"
+    },
+  
+    {
+        "title":"Shikar India: Air India Poster",
+        "Type":"Poster",
+        "Creator":"K. K. Save",
+        "Year":"1968",
+        "Relevance":"Indian Advertising",
+        "Intensity":6,
+        "imgURL":"assets/sip.jpg",
+        "readMore":"https://posterhouse.org/exhibition/air-indias-maharaja-advertising-gone-rogue/"
+    },
+
+    {
+        "title":"Raga Todi: Air India Post Card",
+        "Type":"Post Card",
+        "Creator":"Avinash Godbole",
+        "Year":"1975",
+        "Relevance":"Interdisciplinary Contextualisation",
+        "Intensity":5,
+        "imgURL":"assets/aipc.jpg",
+        "readMore":"https://posterhouse.org/exhibition/air-indias-maharaja-advertising-gone-rogue/"
+    },
+
+    {
+        "title":"Authority and Identity in India",
+        "Type":"Essay",
+        "Creator":"T. G. Vaidyanathan",
+        "Year":"1989",
+        "Relevance":"Indian Identity",
+        "Intensity":5,
+        "imgURL":"assets/aii.jpg",
+        "readMore":"https://www.jstor.org/stable/20025268"
+    },
+
+    {
+        "title":"When Was Modernism",
+        "Type":"Book",
+        "Creator":"Geeta Kapur",
+        "Year":"2000",
+        "Relevance":"Indian Art History",
+        "Intensity":4,
+        "imgURL":"assets/wwm.jpg",
+        "readMore":"https://books.google.com/books/about/When_was_Modernism.html?id=NxGTNwAACAAJ&source=kp_book_description"
+    },
+
+    {
+        "title":"South Asian Technospaces",
+        "Type":"Book",
+        "Creator":["Radhika Gajjala", "Venkataramana Gajjala"],
+        "Year":"2008",
+        "Relevance":"Internet and India",
+        "Intensity":4,
+        "imgURL":"assets/sat.jpg",
+        "readMore":"https://books.google.com/books/about/South_Asian_Technospaces.html?id=cMZFoROURUQC"
+    },
+
+    {
+        "title":"Handpainted Signage",
+        "Type":"Signage",
+        "Creator":"Unknown Artisan",
+        "Year":"2012",
+        "Relevance":"Visual Vernacular",
+        "Intensity":3,
+        "imgURL":"assets/hs.jpg",
+        "readMore":"https://www.designsquabble.com/post/painted-signs-of-india"
+    },
+
+
+    {
+        "title":"Swadeshi Color",
+        "Type":"Essay",
+        "Creator":"Natasha Eaton",
+        "Year":"2013",
+        "Relevance":"Indian Art History",
+        "Intensity":3,
+        "imgURL":"assets/sc.jpg",
+        "readMore":"https://www.jstor.org/stable/43188857"
+    },
+
+    {
+        "title":"Blockprinted Textiles of India: Imprints of Culture",
+        "Type": "Book",
+        "Creator": "Eiluned Edwards",
+        "Year": "2016",
+        "Relevance": "Craft Culture",
+        "Intensity": 3,
+        "imgURL": "assets/bti.jpg",
+        "readMore": "https://books.google.com/books/about/Block_Printed_Textiles_of_India.html?id=j7_cjgEACAAJ&source=kp_book_description"
+    },
+
+    {
+        "title":"Truck Art",
+        "Type":"Craft",
+        "Creator":"Trucking Community",
+        "Year":"2021",
+        "Relevance":"Visual Vernacular",
+        "Intensity":1,
+        "imgURL":"assets/ta.jpg",
+        "readMore":"https://hyperallergic.com/649642/india-vibrant-idiosyncratic-truck-art/"
+    },
+
+    {
+        "title":"Bandish Blues",
+        "Type":"Song",
+        "Creator":"The Darshan Doshi Trio",
+        "Year":"2021",
+        "Relevance":"Interdisciplinary Contextualisation",
+        "Intensity":4,
+        "imgURL":"assets/bb.jpg",
+        "readMore":"https://www.youtube.com/watch?v=hHReQQfIXkk"
+    },
+
+    {
+        "title":"Metamorphosis",
+        "Type":"Installation",
+        "Creator": ["Sameer Kulavoor", "Sandeep Meher"],
+        "Year":"2022",
+        "Relevance":"Visual Vernacular",
+        "Intensity":3,
+        "imgURL":"assets/met.jpg",
+        "readMore":"https://www.outlookindia.com/art-entertainment/a-world-of-creative-depiction-at-mumbai-urban-art-festival-photos-261215"
+    },
+ 
+    {
+        "title":"Mehram",
+        "Type":"Song",
+        "Creator":["Asfar Hussain", "Arooj Aftab"],
+        "Year":"2022",
+        "Relevance":"Interdisciplinary Contextualisation",
+        "Intensity":4,
+        "imgURL":"assets/meh.jpg",
+        "readMore":"https://www.youtube.com/watch?v=BUm_hFMlsgg"
     }
-    placeholder,innerHTML = out;
-})
+
+    ];    
+
+let dataContainerA = document.querySelector("#data-container-A");
+let dataContainerB = document.querySelector("#data-container-B");
+let dataContainerC = document.querySelector("#data-container-C");
+let dataContainerD = document.querySelector("#data-container-D");
+let modalContainer = document.querySelector("#modal-container");
+
+
+
+
+for (let x of dataJSON){
+
+let dataHTML = `<div class="data">
+                    <div class="data-bckg" style="background-image: url(${x.imgURL})"></div>
+                </div>`;
+let modalHTML = `<div class="modal-wrapper">
+                    <div class="modal">
+                        <div class="data-img" style="background-image: url(${x.imgURL})">
+                        </div>
+                        <div class="data-info">
+                            <div class="data-name">
+                                <h2>${x.title}</h2>
+                            </div>
+                            <div class="data-other-info">
+                                <div class="data-year">
+                                    <p>${"Year: " + x.Year}</p>
+                                </div>
+                                <div class="data-type">
+                                    <p>${"Ephemera Type: " + x.Type}</p>
+                                </div>
+                                <div class="data-designer">
+                                    <p>${"Designer(s): " + x.Creator}</p>
+                                </div>
+                                <div class="data-relevance">
+                                    <p>${"Relevance: " + x.Relevance}</p>
+                                </div>
+                                <div class="data-url">
+                                    <a href="${x.readMore}">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+let newData = document.createElement("div");
+let newModal = document.createElement("div");
+
+let i = dataJSON.indexOf(x);
+
+if (x.Year.slice(0, 2) == "15" || x.Year.slice(0, 2) == "16" || x.Year.slice(0, 2) == "17") {
+
+    dataContainerA.appendChild(newData);
+    newData.innerHTML = dataHTML;
+    modalContainer.appendChild(newModal);
+    newModal.innerHTML = modalHTML;
+    newData.style.height = (x.Intensity * 50) + "px";
+    newData.style.width = (x.Intensity * 50) + "px";
+    newData.style.margin = "0 -2.5%";
+
+    newData.classList.add("data-" + [i]);
+    newModal.classList.add("modal-" + [i])
+
+}
+
+if (x.Year.slice(0, 2) == "18") {
+
+    dataContainerB.appendChild(newData);
+    newData.innerHTML = dataHTML;
+    modalContainer.appendChild(newModal);
+    newModal.innerHTML = modalHTML;
+    newData.style.height = (x.Intensity * 50) + "px";
+    newData.style.width = (x.Intensity * 50) + "px";
+    newData.style.margin = "0 -2.5%";
+
+    newData.classList.add("data-" + [i]);
+    newModal.classList.add("modal-" + [i]);
+
+    
+}
+
+if (x.Year.slice(0, 2) == "19") {
+
+    dataContainerC.appendChild(newData);
+    newData.innerHTML = dataHTML;
+    modalContainer.appendChild(newModal);
+    newModal.innerHTML = modalHTML;
+    newData.style.height = (x.Intensity * 50) + "px";
+    newData.style.width = (x.Intensity * 50) + "px";
+    newData.style.margin = "0 -2.5%";
+
+    newData.classList.add("data-" + [i]);
+    newModal.classList.add("modal-" + [i]);
+}
+
+else if (x.Year.slice(0, 2) == "20") {
+
+    dataContainerD.appendChild(newData);
+    newData.innerHTML = dataHTML;
+    modalContainer.appendChild(newModal);
+    newModal.innerHTML = modalHTML;
+    newData.style.height = (x.Intensity * 50) + "px";
+    newData.style.width = (x.Intensity * 50) + "px";
+    newData.style.margin = "0 -2.5%";
+
+    newData.classList.add("data-" + [i]);
+    newModal.classList.add("modal-" + [i]);
+}
+
+}
+
+
+
+
+
+
+
+let dataDivs = document.querySelectorAll(".data");
+console.log("this is the test for: " + dataDivs[9]);
+
+
+dataDivs.forEach(addEventListener("click", function(){
+    dataDivs[x].onclick = function(){
+        getElementsByClassName("modal-" + [x]).style.display = "block";
+
+    }
+
+    this.window.onclick = function(){
+        if (event.target == getElementsByClassName("modal-" + [x]))
+{
+    getElementsByClassName("modal-" + [x]).style.display = "none";
+}    }
+
+}))
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+// var thisData = document.getElementsByClassName("data-" + [i]);
+// var thisModal = document.getElementsByClassName("modal-" + [i]);
+// data.addEventListener("click", function(){
+
+// thisData.onclick = function() {
+// thisModal.style.display = "block";
+// console.log("modal displayed");
+// }
+// window.onclick = function() {
+// if (event.target == thisModal) {
+//     thisModal.style.display = "none";
+// }}
+// })
+
+
+
+
+
+
+
+
+
+// var modalWrapper = document.querySelector(".modal-wrapper");
+// var modalcta = document.querySelector(".data");
+
