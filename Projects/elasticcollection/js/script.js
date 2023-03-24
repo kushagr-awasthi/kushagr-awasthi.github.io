247b79795c2401b71ad0a6fabca9e43cf57b7e79
kushagr-awasthi <112492988+kushagr-awasthi@users.noreply.github.com>
2023-03-23 20:05:42 -0400

    Post-Crit Changes

--- Projects/elasticcollection/js/script.js
+++ Projects/elasticcollection/js/script.js
@@ -1,4 +1,5 @@
-var dataJSON = [
+// ----------------------------------------------------------DATA
+var rawdataJSON = [
 
     {
         "title":"Ajrakh Blockprints",
@@ -105,7 +106,7 @@ var dataJSON = [
         "title":"Commemorative Front Page, The Hindu",
         "Type": "News Graphic",
         "Creator": "Unknown Designer",
-        "Year":1940,
+        "Year":1947,
         "Relevance": "Civic Identity",
         "Intensity": 6,
         "imgURL":"assets/hfp.jpg",
@@ -287,10 +288,135 @@ var dataJSON = [
         "Intensity":4,
         "imgURL":"assets/meh.jpg",
         "readMore":"https://www.youtube.com/watch?v=BUm_hFMlsgg"
+    },
+    {
+        "title":"The Bombay Songstress",
+        "Type":"Painting",
+        "Creator": "Raja Ravi Varma",
+        "Year":1893,
+        "Relevance":"Spirit",
+        "Intensity":4,
+        "imgURL":"assets/tbs.jpg",
+        "readMore":"https://artsandculture.google.com/asset/bombay-singer-bombay-songstress-raja-ravi-varma/NwHU2dT1xTFc-Q?hl=en"
+    },
+     {
+        "title":"Textiles and Ornaments of India",
+        "Type":"Book",
+        "Creator": ["Pupul Jayakar", " John Erwin"],
+        "Year":1956,
+        "Relevance":"Aesthetic Heritage",
+        "Intensity":2,
+        "imgURL":"assets/toi.jpg",
+        "readMore":"https://www.moma.org/documents/moma_catalogue_3327_300062146.pdf"
+    },
+    {
+        "title":"India's Craft Tradition",
+        "Type":"Book Excerpt",
+        "Creator": "Kamaladevi Chattopadhyay",
+        "Year":1980,
+        "Relevance":"Craft Culture",
+        "Intensity":2,
+        "imgURL":"assets/ict.jpg",
+        "readMore":"https://www.jstor.org/stable/23005531?seq=6"
+    },
+    {
+        "title":"Indian Modernism via an Eclectic and Elusive Artist",
+        "Type":"Article",
+        "Creator": "Holland Cotter",
+        "Year":2008,
+        "Relevance":"Power of Design",
+        "Intensity":6,
+        "imgURL":"assets/imv.jpg",
+        "readMore":"https://www.nytimes.com/2008/08/20/arts/design/20bose.html"
+    },
+    {
+        "title":"Doordarshan Logo",
+        "Type":"Logo",
+        "Creator": "Devashish Bhattacharya",
+        "Year":1970,
+        "Relevance":"Indian Branding",
+        "Intensity":1,
+        "imgURL":"assets/ddl.jpg",
+        "readMore":"https://m.economictimes.com/news/politics-and-nation/doordarshan-turns-60-here-is-a-nostalgic-look-at-the-broadcasters-milestones/iconic-logo/slideshow/71241731.cms"
+    },
+    {
+        "title":"Mumbai International Airport",
+        "Type":"Architecture",
+        "Creator": "Skidmore Owings Merril",
+        "Year":2014,
+        "Relevance":"21<sup>st</sup> Century India",
+        "Intensity":3,
+        "imgURL":"assets/mia.jpg",
+        "readMore":"https://www.nytimes.com/2019/03/12/arts/mumbai-airport-jaya-he-gvk-museum.html"
+    },
+    {
+        "title":"Parle G Packaging",
+        "Type":"Packaging",
+        "Creator": "Everest Creative Agency",
+        "Year":1960,
+        "Relevance":"Indian Branding",
+        "Intensity":2,
+        "imgURL":"assets/plg.jpg",
+        "readMore":"https://economictimes.indiatimes.com/advertising-marketing/parle-g-the-journey-of-a-biscuit-for-masses/whos-that-girl/slideshow/24922383.cms"
+    },
+    {
+        "title":"Camel's Flesh Tint",
+        "Type":"Art Supplies",
+        "Creator": "Unknown",
+        "Year":2005,
+        "Relevance":"Western Fetish",
+        "Intensity":4,
+        "imgURL":"assets/flt.jpg",
+        "readMore":"https://www.amazon.in/s?k=camel+flesh+tint&crid=3BPW5NO3S3PPB&sprefix=camel+flesh+tint%2Caps%2C171&ref=nb_sb_noss_2"
+    },
+    {
+        "title":"Royal Opera House, Mumbai",
+        "Type":"Architecture",
+        "Creator": ["Maurice Bandmann", " Jehangir Framji Karaka"],
+        "Year":1912,
+        "Relevance":"Colonial Imagery",
+        "Intensity":4,
+        "imgURL":"assets/roh.jpg",
+        "readMore":"https://artsandculture.google.com/story/india-s-only-surviving-opera-house-the-royal-opera-house-mumbai/MwWBPMjCigNyIQ?hl=en"
+    },
+    {
+        "title":"The Company Style",
+        "Type":"Art",
+        "Creator": "Unknown",
+        "Year":1835,
+        "Relevance":"Colonial Imagery",
+        "Intensity":5,
+        "imgURL":"assets/tcs.jpg",
+        "readMore":"https://www.metmuseum.org/art/collection/search/453350"
+    },
+    {
+        "title":"Mill Labels",
+        "Type":"Packaging",
+        "Creator": "Unknown",
+        "Year":1937,
+        "Relevance":"Indian Branding",
+        "Intensity":4,
+        "imgURL":"assets/ml.jpg",
+        "readMore":"https://www.hindustantimes.com/art-and-culture/check-out-these-10-vintage-prints-from-the-pre-independence-era/story-4lKP0HuLkKPIfSJKQ8N3BK.html"
+    },
+    {
+        "title":"Reliefs on the New India Assurance Building",
+        "Type":"Scultpure",
+        "Creator": "Unknown",
+        "Year":1936,
+        "Relevance":"Indian Art Deco",
+        "Intensity":2,
+        "imgURL":"assets/niab.jpg",
+        "readMore":"https://www.joinpaperplanes.com/swadeshi-sculptures-on-the-new-india-assurance-company-building-in-fort-mumbai/"
     }
 
-    ];    
-var dataJSON = dataJSON.sort((a, b) => (a.Year > b.Year) ? 1 : -1);
+    ];   
+//sort Data chronologically 
+var dataJSON = rawdataJSON.sort((a, b) => (a.Year > b.Year) ? 1 : -1);
+
+//-------------------------------------------- DISPLAYING THE DATA
+
+// Getting Data Containers
 let dataContainerA = document.querySelector("#data-container-A");
 let dataContainerB = document.querySelector("#data-container-B");
 let dataContainerC = document.querySelector("#data-container-C");
@@ -298,13 +424,13 @@ let dataContainerD = document.querySelector("#data-container-D");
 let modalContainer = document.querySelector("#modal-container");
 
 
-
-
+// Loop through Data (Object Array).
 for (let x of dataJSON){
-
+// Create a variable (dataHTML) which is a template literal which draws specific background image values from the data array.
 let dataHTML = `
                     <div class="data-bckg" style="background-image: url(${x.imgURL})"></div>
                 `;
+// Create a variable (modalHTML)which is a template literal which draws specific values for the type, relevance, readMore, creator and year.
 let modalHTML = `
                     <div class="modal">
                         <div class="data-img" style="background-image: url(${x.imgURL})">
@@ -333,61 +459,80 @@ let modalHTML = `
                         </div>
                     </div>
                 `;
+// For Each data point create 2 new Divs- newData (Circle) and newModal (Pop-up).
 let newData = document.createElement("div");
 let newModal = document.createElement("div");
+// Creating a new i variable which is equal to the indexof the current data point being looped over.
 let i = dataJSON.indexOf(x);
+// to each newData Div add the class data-i (data-1, data-2 and so on) and also the generic class data.
 newData.classList.add("data-" + [i]);
 newData.classList.add("data");
+// to each newModal Div add the class modal-i (modal-1, modal-2 and so on) and also the generic class modal-wrapper
 newModal.classList.add("modal-" + [i]);
 newModal.classList.add("modal-wrapper");
+// In each newData div, insert the dataHTML
 newData.innerHTML = dataHTML;
+// In each newModal div, insert the modalHTML
 newModal.innerHTML = modalHTML;
+// for each newData div set the height and width to the intensity of that div's data multiplied by 40px
 newData.style.height = (x.Intensity * 40) + "px";
 newData.style.width = (x.Intensity * 40) + "px";
+// for each newData div set the margin to the intensity of that div's data multiplied by 2.5px
 newData.style.margin = ((x.Intensity * 2.5) + "px") ;
+// Add the newModal div into the modal container
 modalContainer.appendChild(newModal);
-
-
+//there are 4 data containers for <2100, <2000, <1900, <1800
+//Using an if else statement add Data whose year value is less than 1800 to container A
 if (x.Year < 1800) {
     dataContainerA.appendChild(newData);
 }
-
+// Data whose year value is less than 1900 and greater than 1800 to container B
 if (x.Year < 1900 && x.Year >= 1800) {
     dataContainerB.appendChild(newData);    
 }
-
+//Data whose year value is less than 2000 and greater than 1900 to container C
 if (x.Year < 2000 && x.Year >= 1900) {
     dataContainerC.appendChild(newData);
 }
-
+//Data whose year value is less than 2100 and greater than 2000 to container D
 else if (x.Year < 2100 && x.Year >= 2000) {
     dataContainerD.appendChild(newData);
 }
-
 }
 
-for (let i = 0; i < dataJSON.length; i++){
 
+
+//-------------------------------------------------------------CONNECT DATA TO MODALS SO THAT CLICKING OPENS THE MODAL
+
+//Create a for let loop that does a particular action the number of times as the number of data
+for (let i = 0; i < dataJSON.length; i++){
+// create a variable which gets elements with the class data-i and modal-i for ex. get data-1 and modal-1, then data-2 and modal-2 and so on until you get through all the data
     let thisData = document.querySelector(".data-" + [i]);
     let thisModal = document.querySelector(".modal-" + [i]);
+// to the current data (thisData) add a "click" event listener 
     thisData.addEventListener("click", function(){
+// On click set the display of the corresponding modal (i is the same thus modal-1 and data-1 will always correspond) to "block" (i.e. visible)
         thisModal.style.display = "block";
+// Close the modal when clicking on the window outside the modal
         window.onclick = function(){
             if (event.target == thisModal) {
                 thisModal.style.display = "none";
             }
         }
     })
-   
-
   }
 
 
-  
+//----------------------------------------------------------------SET UP CONTROL PANEL
+
+//Set up variable for the control panel button and the control-panel pop-up (control-modal-wrapper)
 let controlButton = document.getElementById("control-button");
 let controlModal = document.getElementById("control-modal-wrapper");
+// Add a "click" event listener to the button
 controlButton.addEventListener("click", function(){
+// On click set the display of the control modal to "block" (i.e. visible)
     controlModal.style.display = "block";
+// Close the modal when clicking on the window outside the modal
     window.onclick = function(){
         if (event.target == controlModal) {
             controlModal.style.display = "none";
@@ -395,17 +540,22 @@ controlButton.addEventListener("click", function(){
     }
 })
 
+//-------------------------------------------------------------------FILTERS: EQUALIZER
 
+// Set up an equalize toggle(in index.html) which equalizes the size of all the data
+
+// set up a variable which gets all the data i.e. all elements with the "data" class.
 var allData = document.querySelectorAll(".data");
+// set up a variable which gets the size toggle switch.
 var sizeToggle = document.getElementById("size-toggle");
 
-
+// add a "change" event listener to the toggle switch
 sizeToggle.addEventListener('change', function(){
 
-
+// Using an if/else statement...
 if  (this.checked) {
 
-    
+// if the switch is on (checked) Loop through all of the data divs and set the size and width to 120px.  
     for(let i = 0; i < dataJSON.length; i++){
         allData[i].style.width = "120px";
         allData[i].style.height = "120px";
@@ -414,58 +564,133 @@ if  (this.checked) {
 }
 
  if (this.checked == false) {
+// if the switch is off (unchecked) Loop through all of the data divs and set the size and width to the intensity multiplied by 40px.  
     for(let i = 0; i < dataJSON.length; i++){
         allData[i].style.width = (dataJSON[i].Intensity * 40) + "px";
         allData[i].style.height = (dataJSON[i].Intensity * 40) + "px";
     }
          console.log("data sized by intensity");
+
    }
+
  })
 
+//-------------------------------------------------------------------------FILTERS: BY YEAR
+
+
+//Set up 2 sliders (in index.html) which allow users to filter by max and min year.
+// Set up variables that get the min and max sliders
  var minYear = document.getElementById("minyear-slider");
  var maxYear = document.getElementById("maxyear-slider");
+ // Create an empty array where we will put in all the years of the data
  var yearArray = [];
  
-
+// using a for let loop, iterate through our data and push(add) every year value into the year array
 for(let i = 0; i < dataJSON.length; i++){
     yearArray.push(dataJSON[i].Year);
 
  };
-
+// Create two Variables that get the Year Callouts on the main page.
+ var navMsA = document.getElementById("navp1");
+var navMsB = document.getElementById("navp2");
+// Sort the Year Array in ascending order. i.e. chronological.
  var yearArray = yearArray.sort((a, b) => (a > b) ? 1 : -1);
+ // Set up a new variable called m (max) which gives the # of the last year value. -1 because array.length will start count from 0
  let m = yearArray.length - 1;
  
+ // Set the min and max range of the minimum Year Slider to 0 and m. Set the default value to 0 (oldest Year).
  minYear.min = yearArray[0];
  minYear.max = yearArray[m];
  minYear.value = yearArray[0];
+  // Set the min and max range of the maximum Year Slider to 0 and m. Set the default value to m (newest Year).
  maxYear.min = yearArray[0];
  maxYear.max = yearArray[m];
  maxYear.value = yearArray[m];
+ // Add the 0 and m values to the year callouts on the homepage.
+ navMsA.innerHTML = yearArray[0];
+ navMsB.innerHTML = yearArray[m];
 
- var usedata = [];
- 
+// Add a change event listener to the minimum year slider 
 minYear.addEventListener("change", function(){
+// Set the Year Callout in the control pop up the user's selection(value)
 document.getElementById("minselection").innerHTML = minYear.value; 
+// Set the Year Callout on the main page to the user's selection(value)
+navMsA.innerHTML = minYear.value;
+navMsB.innerHTML = maxYear.value;
 console.log("now displaying entries dated from: " + minYear.value + " to: " + maxYear.value);
+console.log("Nav Bar Resized due to Min Year Selection");
+// Create a for let loop that runs the same number of times as the number of data.
 for(let i = 0; i < dataJSON.length; i++) {
+// Create a variable which fetches each data one by one using the counter [i]
          filteredData = document.querySelector(".data-"+ [i]);
+// Using an if else statement check if the data corresponding to the div is greater than or equal to the min year value AND also less than or equal to the max year value.
 if (dataJSON[i].Year >= minYear.value && dataJSON[i].Year <= maxYear.value) {
+// If true then change display to block (visible).
 filteredData.style.display = "block";
 }
+//if false then change display to none (invisible).
 else filteredData.style.display = "none";
 
 }
+
+nav.style.height = dataConHeight;
 })
- 
+// Add a change event listener to the maximum year slider 
 maxYear.addEventListener("change", function(){
- 
+// Set the Year Callout in the control pop up the user's selection(value) 
 document.getElementById("maxselection").innerHTML = maxYear.value; 
+// Set the Year Callout on the main page to the user's selection(value)
+navMsA.innerHTML = minYear.value;
+navMsB.innerHTML = maxYear.value;
 console.log("now displaying entries dated from: " + minYear.value + " to: " + maxYear.value);
+console.log("Nav Bar Resized due to Max Year Selection");
+// Create a for let loop that runs the same number of times as the number of data.
 for(let i = 0; i < dataJSON.length; i++) {
+// Create a variable which fetches each data one by one using the counter [i]
         let filteredData = document.querySelector(".data-"+ [i]);
+// Using an if else statement check if the data corresponding to the div is greater than or equal to the min year value AND also less than or equal to the max year value.
 if (dataJSON[i].Year >= minYear.value && dataJSON[i].Year <= maxYear.value) {
+// If true then change display to block (visible).
 filteredData.style.display = "block";
 }
+//if false then change display to none (invisible).
 else filteredData.style.display = "none";
  }
+
+// Create new variables that fetch the nav (the one with the arrow and years) and the data container wrapper
+//Then get the style attributes of the Data Container and specifically its height
+//Set the nav's height to the Data Container's Height (in CSS, Min-Height limits the minimum to the viewport height).
+
+// Create new variables that fetch the nav (the one with the arrow and years) and the data container wrapper
+//Then get the style attributes of the Data Container and specifically its height
+//Set the nav's height to the Data Container's Height (in CSS, Min-Height limits the minimum to the viewport height).
+ let nav = document.querySelector("nav");
+ let dataCon = document.querySelector("#data-containers-wrapper");
+ let dataConStyle = getComputedStyle(dataCon);
+ let dataConHeight = dataConStyle.height;
+ nav.style.height = dataConHeight;
+
 })
+
+
+//-------------------------------------------------------NAV BAR HEIGHT
+
+
+// Set the Nav height to the container height by default.
+
+let nav = document.querySelector("nav");
+let dataCon = document.querySelector("#data-containers-wrapper");
+let dataConStyle = getComputedStyle(dataCon);
+let dataConHeight = dataConStyle.height;
+nav.style.height = dataConHeight;
+
+// Set the Nav height to the container height if the window is resized.
+
+window.onresize = function() {
+    let nav = document.querySelector("nav");
+let dataCon = document.querySelector("#data-containers-wrapper");
+let dataConStyle = getComputedStyle(dataCon);
+let dataConHeight = dataConStyle.height;
+nav.style.height = dataConHeight;
+console.log("Nav Bar Resized due to Window Resize");
+};
