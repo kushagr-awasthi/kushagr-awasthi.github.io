aa5921432f0e5fd426f857ed505db594627fca67
kushagr-awasthi <112492988+kushagr-awasthi@users.noreply.github.com>
2024-09-06 09:33:00 -0400

    added thesis assignment one

---
+++ Projects/Thesis/256Images/block view/script.js
@@ -0,0 +1,154 @@
+
+const graciousImages = [],
+      openImages = [],
+      opinionatedImages = [],
+      ornamentedImages = [],
+      rootedImages = [];
+
+const parentDiv = document.getElementById("parent-div");
+
+
+
+const constructImageArray = (arrayName, array, index) => {     
+    for (let i = 0; i < index; i++){
+    let imageLink = `../Images/${arrayName}/${arrayName}_${(i+1).toString().padStart(2,"0")}.webp`;
+    array.push(imageLink);
+}
+}
+
+constructImageArray("gracious", graciousImages, 43);
+constructImageArray("open", openImages, 50);
+constructImageArray("opinionated", opinionatedImages, 57);
+constructImageArray("ornamented", ornamentedImages, 61);
+constructImageArray("rooted", rootedImages, 40);
+
+
+
+const collatedImages = [];
+
+const createObjectArray = (facet, array, index) => {
+    
+    for (let i = 0; i < index; i++){
+        let obj = {};
+        obj.url = array[i];
+        obj.facet = facet;
+        collatedImages.push(obj);
+    }
+}
+
+
+
+createObjectArray("gracious", graciousImages, 43);
+createObjectArray("open", openImages, 50);
+createObjectArray("opinionated", opinionatedImages, 57);
+createObjectArray("ornamented", ornamentedImages, 61);
+createObjectArray("rooted", rootedImages, 40);
+
+function shuffle(array) {
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  }
+
+  shuffle(collatedImages);
+
+
+  for (let i = 0; i < collatedImages.length; i++){
+    let child = document.createElement("div");
+    child.classList.add("child-div");
+    if (collatedImages[i].facet == "gracious"){
+        child.style.backgroundColor = "#d3b9d9";
+        child.classList.add("gracious");
+    } else if (collatedImages[i].facet == "open"){
+        child.style.backgroundColor = "#8cd1ed";
+        child.classList.add("open");
+    } else if (collatedImages[i].facet == "ornamented"){
+        child.style.backgroundColor = "#f4785c";
+        child.classList.add("ornamented");
+    } else if (collatedImages[i].facet == "opinionated"){
+        child.style.backgroundColor = "#ffc430";
+        child.classList.add("opinionated");
+    } else if (collatedImages[i].facet == "rooted"){
+        child.style.backgroundColor = "#b5e78d";
+        child.classList.add("rooted");
+    };
+    child.innerHTML = `<img src="${collatedImages[i].url}">`;
+    parentDiv.appendChild(child);
+  }
+
+  let childDivs = document.querySelectorAll(".child-div");
+  let rtCheckbox = document.getElementById("rt-checkbox"),
+      opCheckbox = document.getElementById("op-checkbox"),
+      oiCheckbox = document.getElementById("oi-checkbox"),
+      orCheckbox = document.getElementById("or-checkbox"),
+      grCheckbox = document.getElementById("gr-checkbox");
+
+
+rtCheckbox.addEventListener("change", function(){
+if(rtCheckbox.checked){
+    for (let i = 0; i < childDivs.length; i++){
+        if (childDivs[i].classList.contains("rooted")){
+            childDivs[i].style.display = "block";}
+        
+}} else {
+    for (let i = 0; i < childDivs.length; i++){
+        if (childDivs[i].classList.contains("rooted")){
+            childDivs[i].style.display = "none";}
+}
+}});
+
+opCheckbox.addEventListener("change", function(){
+    if(opCheckbox.checked){
+        for (let i = 0; i < childDivs.length; i++){
+            if (childDivs[i].classList.contains("open")){
+                childDivs[i].style.display = "block";}
+            
+    }} else {
+        for (let i = 0; i < childDivs.length; i++){
+            if (childDivs[i].classList.contains("open")){
+                childDivs[i].style.display = "none";}
+    }
+    }});
+
+    oiCheckbox.addEventListener("change", function(){
+        if(oiCheckbox.checked){
+            for (let i = 0; i < childDivs.length; i++){
+                if (childDivs[i].classList.contains("opinionated")){
+                    childDivs[i].style.display = "block";}
+                
+        }} else {
+            for (let i = 0; i < childDivs.length; i++){
+                if (childDivs[i].classList.contains("opinionated")){
+                    childDivs[i].style.display = "none";}
+        }
+        }});
+
+        orCheckbox.addEventListener("change", function(){
+            if(orCheckbox.checked){
+                for (let i = 0; i < childDivs.length; i++){
+                    if (childDivs[i].classList.contains("ornamented")){
+                        childDivs[i].style.display = "block";}
+                    
+            }} else {
+                for (let i = 0; i < childDivs.length; i++){
+                    if (childDivs[i].classList.contains("ornamented")){
+                        childDivs[i].style.display = "none";}
+            }
+            }});
+
+            grCheckbox.addEventListener("change", function(){
+                if(grCheckbox.checked){
+                    for (let i = 0; i < childDivs.length; i++){
+                        if (childDivs[i].classList.contains("gracious")){
+                            childDivs[i].style.display = "block";}
+                        
+                }} else {
+                    for (let i = 0; i < childDivs.length; i++){
+                        if (childDivs[i].classList.contains("gracious")){
+                            childDivs[i].style.display = "none";}
+                }
+                }});
--- Projects/Thesis/256Images/script.js
+++ Projects/Thesis/256Images/script.js
@@ -2,7 +2,10 @@ const controlButton = document.getElementById("control-button"),
     controlCenter = document.getElementById("control-center"),
     controlCloseButton = document.getElementById("control-close-button"),
     clearCanvasButton = document.getElementById("clear-canvas-button"),
-    brushSizeSlider = document.getElementById("brush-sizer");
+    brushSizeSlider = document.getElementById("brush-sizer"),
+    speedSlider = document.getElementById("speed-slider"),
+    coloriseCheckbox = document.getElementById("colorise-checkbox");
+    mainCTA = document.getElementById("main-cta");
 
 let controlVisible = false;
 
@@ -84,15 +87,9 @@ createObjectArray("rooted", rootedImages, 40);
 
 function shuffle(array) {
     let currentIndex = array.length;
-  
-    // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
-      // Pick a remaining element...
       let randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
-      // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
@@ -101,6 +98,7 @@ function shuffle(array) {
   shuffle(collatedImages);
 
 function setRectColor(facet) {
+    if (coloriseCheckbox.checked == true){
     if (facet == "gracious"){
         return '#d3b9d9';
     } else if (facet == "open"){
@@ -112,56 +110,165 @@ function setRectColor(facet) {
     } else if (facet == "rooted"){
         return '#b5e78d';
     } else return '#ffffff';
+} else return '#ffffff00';
+
 }
 
 let currentImageIndex = 0;
+
 let imgWidth = brushSizeSlider.value;
 brushSizeSlider.addEventListener("input", function(){
     imgWidth = brushSizeSlider.value;
     return imgWidth;
+});
+let speedFactor = speedSlider.value / 100;
+speedSlider.addEventListener("input", function(){
+    speedFactor = speedSlider.value / 100;
+    return speedFactor;
 })
+function calculateImageHeight(img, imgWidth) {
+    if (img.complete) {
+        const naturalWidth = img.naturalWidth;
+        const naturalHeight = img.naturalHeight;
+        const aspectRatio = naturalHeight / naturalWidth;
+        const imgHeight = imgWidth * aspectRatio;
+        return imgHeight;
+    } else {
+        console.log("Image not loaded yet.");
+        return null;
+    }
+}
 
+let particles = [];
+let imgHeight = imgWidth;
 
-function drawImage(e) {
-    let img = new Image();
-    img.src = collatedImages[currentImageIndex].url;
-    img.onload = function() {
-        
-        const imgHeight = (img.naturalHeight / img.naturalWidth) * imgWidth;
-        ctx.drawImage(img, (e.clientX - (imgWidth/2)), (e.clientY- (imgHeight/2)), imgWidth, imgHeight);
-        ctx.save();
-        ctx.globalCompositeOperation = 'color';
-        const rectangleColor = setRectColor(collatedImages[currentImageIndex].facet);
-        ctx.fillStyle = rectangleColor;
-        ctx.fillRect((e.clientX - (imgWidth/2)), (e.clientY- (imgHeight/2)), imgWidth, imgHeight);
-        ctx.restore();
-        currentImageIndex = currentImageIndex + 1;
-        if (currentImageIndex == 251){
-            currentImageIndex = 0;
+class Particle{
+    constructor( x, y, imgSrc, velocityX, velocityY, facet, lifespan = 1000000) {
+        this.x = x;
+        this.y = y;
+        this.img = new Image();
+        this.img.src = imgSrc;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.facet = facet;
+        this.lifespan = lifespan;
+        this.creationTime = Date.now();
+    }
+
+    update() {
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+        let elapsedTime = Date.now() - this.creationTime;
+        this.lifespan -= elapsedTime;
+    }
+
+    draw() {
+        if(this.img.complete) {
+            imgHeight = calculateImageHeight(this.img, imgWidth);
+            ctx.drawImage(this.img, this.x - imgWidth / 2, this.y - imgHeight / 2, imgWidth, imgHeight);
+            ctx.save();
+            ctx.globalCompositeOperation = 'color';
+            const rectangleColor = setRectColor(this.facet);
+            ctx.fillStyle = rectangleColor;
+            ctx.fillRect(this.x - imgWidth / 2, this.y - imgHeight / 2, imgWidth, imgHeight);
+            ctx.restore();
         }
-    };
+    }
+
+    isExpired() {
+        return this.lifespan <= 0;
+    }
+}
+
+let prevX = 0;
+let prevY = 0;
+
+function createParticle(e) {
+    const currentImage = collatedImages[currentImageIndex];
+    const imgSrc = currentImage.url;
+    let velocityX = (prevX - e.clientX) * speedFactor;
+    let velocityY = (prevY - e.clientY) * speedFactor;
+
+
+    const particle = new Particle(e.clientX, e.clientY, imgSrc, velocityX, velocityY, currentImage.facet, 1000000);
+    particles.push(particle);
+
+
+    currentImageIndex = (currentImageIndex + 1) % collatedImages.length;
+
+    prevX = e.clientX;
+    prevY = e.clientY;
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach((particle, index) => {
+        particle.update();
+        particle.draw();
+    });
+
+    for (let i = particles.length - 1; i >= 0; i--) {
+        const particle = particles[i];
+        particle.update();
+        if (particle.isExpired()) {
+            particles.splice(i, 1);
+        } else {
+            particle.draw();
+        }
+    }
+    requestAnimationFrame(animate);
+}
+canvas.addEventListener('mousemove', (e) => {
+    createParticle(e);
+});
+
+canvas.addEventListener("click", function () {
+    mainCTA.style.opacity = "0";
+animate();
+
+});
+
+
+// --------------WORKING---------------------
+// function drawImage(e) {
+//     let img = new Image();
+//     img.src = collatedImages[currentImageIndex].url;
+//     img.onload = function() {
+//         const imgHeight = (img.naturalHeight / img.naturalWidth) * imgWidth;
+//         ctx.drawImage(img, (e.clientX - (imgWidth/2)), (e.clientY- (imgHeight/2)), imgWidth, imgHeight);
+//         ctx.save();
+//         ctx.globalCompositeOperation = 'color';
+//         const rectangleColor = setRectColor(collatedImages[currentImageIndex].facet);
+//         ctx.fillStyle = rectangleColor;
+//         ctx.fillRect((e.clientX - (imgWidth/2)), (e.clientY- (imgHeight/2)), imgWidth, imgHeight);
+//         ctx.restore();
+//         currentImageIndex = currentImageIndex + 1;
+//         if (currentImageIndex == 251){
+//             currentImageIndex = 0;
+//         }
+//     };
     
    
     
 
 
-}
+// }
 
-function handleMouseEvent(e) {
-    drawImage(e);
-}
+// function handleMouseEvent(e) {
+//     drawImage(e);
+// }
 
-const throttledMouseMove = throttle(handleMouseEvent, 0);
- canvas.addEventListener("click", function(){
- canvas.addEventListener("mousemove", throttledMouseMove);
- clearCanvasButton.style.opacity = "1";
-}, once = true);
+// const throttledMouseMove = throttle(handleMouseEvent, 0);
+//  canvas.addEventListener("click", function(){
+//  canvas.addEventListener("mousemove", throttledMouseMove);
+//  clearCanvasButton.style.opacity = "1";
+// }, once = true);
 
-clearCanvasButton.addEventListener("click", function(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.restore();
-})
+// clearCanvasButton.addEventListener("click", function(){
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.save();
+//     ctx.restore();
+// })
 
 canvas.addEventListener("click", function(){
     if (controlVisible === true){
@@ -170,30 +277,23 @@ canvas.addEventListener("click", function(){
     } else return;
 })
 
-
-
-
-
-
-
-
-function throttle(func, limit) {
-    let lastFunc;
-    let lastRan;
-    return function() {
-        const context = this;
-        const args = arguments;
-        if (!lastRan) {
-            func.apply(context, args);
-            lastRan = Date.now();
-        } else {
-            clearTimeout(lastFunc);
-            lastFunc = setTimeout(function() {
-                if ((Date.now() - lastRan) >= limit) {
-                    func.apply(context, args);
-                    lastRan = Date.now();
-                }
-            }, limit - (Date.now() - lastRan));
-        }
-    };
-}
+// function throttle(func, limit) {
+//     let lastFunc;
+//     let lastRan;
+//     return function() {
+//         const context = this;
+//         const args = arguments;
+//         if (!lastRan) {
+//             func.apply(context, args);
+//             lastRan = Date.now();
+//         } else {
+//             clearTimeout(lastFunc);
+//             lastFunc = setTimeout(function() {
+//                 if ((Date.now() - lastRan) >= limit) {
+//                     func.apply(context, args);
+//                     lastRan = Date.now();
+//                 }
+//             }, limit - (Date.now() - lastRan));
+//         }
+//     };
+// }
